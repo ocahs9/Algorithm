c200a712569ed9cdaaebb8f84bf9b59154204a7a
kong <piorag99@gmail.com>
2024-12-19 23:18:17 +0900

    5주차 알고리즘 풀이

--- Backjoon/week5/19942/main.cpp
+++ Backjoon/week5/19942/main.cpp
@@ -1,71 +1,47 @@
-#include <bits/stdc++.h>
-#include <vector>
-using namespace std;
-
-int n;
-int mp,mf,ms,mv;
-
-vector<int> p;
-vector<int> f;
-vector<int> s;
-vector<int> v;
-vector<int> c;
-
-int accP =0;
-int accF =0;
-int accS =0;
-int accV =0;
-int accC =0;
-
-int totalCount = 0;
-int resultC = 999999999;
-vector<int> resultV;
-
-//시작은 항상 num == 0부터
-void minCost(int num){
-    if(totalCount >= resultC) return; //최소값이 아니니까 바로 종료
-    int elementNum = p.size();
-    if(num >= p.size()) { //이미 끝난 상태(아예 조건을 만족하지 못하는 상태)
-        cout << -1 ;
-        return;
-    }; 
-
-    resultV.push_back(num + 1); 
-    totalCount += c[num];
-    accP += p[num]; accF += f[num]; accS += s[num]; accV += v[num]; accC += c[num];
-
-    if(accP >= mp && accF >= mf && accS >= ms && accV >= mv){ //성공적으로 최소조건을 만족한 경우
-        cout << "야아아아아" << "\n";
-        if(totalCount < resultC) resultC = totalCount;
-
-        cout << totalCount;
-        cout << "\n";
-        for(auto i : resultV) cout << i << " ";
-        return;
-    }
-
-    for(int i=num; i < elementNum; i++){ //다음 것으로 넘어가는 로직 
-        minCost(i);
-    }
-
-    //해당 함수 종료 = 복구 필요
-    accP -= p[num]; accF -= f[num]; accS -= s[num]; accV -= v[num]; accC -= c[num];
-    totalCount -= c[num];
-    resultV.pop_back();
-}
-
+#include<bits/stdc++.h>
+using namespace std;  
+typedef long long ll; 
+const int INF = 987654321;
+int n, mp, mf, ms, mv;
+int b, c, d, e, ret = INF, sum;
+struct A{
+	int mp, mf, ms, mv, cost; 
+}a[16]; 
+map<int, vector<vector<int>>> ret_v;
 int main(){
-    ios_base::sync_with_stdio(0);
-	cin.tie(0); cout.tie(0);
-
-    cin >> n;
-    cin >> mp >> mf >> ms >> mv;
-    for(int i=0; i<n; i++){
-        int tp,tf,ts,tv,tc;
-        cin >> tp >> tf >> ts >> tv >> tc;
-        p.push_back(tp); f.push_back(tf); s.push_back(ts); v.push_back(tv); c.push_back(tc);
-    }
-
-    minCost(0);
-    return 0;
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    cin >> n; 
+    cin >> mp >> mf >> ms >> mv;  
+    for(int i = 0; i < n; i++){
+    	cin >> a[i].mp >> a[i].mf >> a[i].ms >> a[i].mv >> a[i].cost;  
+	}
+	for(int i = 1; i < (1 << n); i++){
+		b = c = d = e = sum = 0;
+        vector<int> v; 
+		for(int j = 0; j < n; j++){
+			if(i & (1 << j)){
+                v.push_back(j + 1);
+				b += a[j].mp;
+				c += a[j].mf;
+				d += a[j].ms;
+				e += a[j].mv;
+				sum += a[j].cost;
+			}
+		}
+		if(b >= mp && c >= mf && d >= ms && e >= mv){
+			if(ret >= sum){
+				ret = sum;
+                ret_v[ret].push_back(v); 
+			}
+		} 
+	}  
+	if(ret == INF) cout << -1 << '\n';
+	else{
+        sort(ret_v[ret].begin(), ret_v[ret].end());  
+		cout << ret << "\n";
+		for(int a : ret_v[ret][0]){
+			cout << a << " ";
+		} 
+	}  
+	return 0; 
 }
